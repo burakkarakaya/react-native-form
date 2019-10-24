e71a153e9ab05d258f32ac82ec3e1097d52e9144
burakkarakaya <burakkarakaya1984@gmail.com>
2019-10-24 17:07:24 +0300

    markalar update

--- app/views/Brands.js
+++ app/views/Brands.js
@@ -13,7 +13,12 @@ const Brand = require('../../data/brands.js');
 class Brands extends PureComponent {
     /* 
         data => tüm markaların bulunduğı data
-        categoryData => gelen datanın içinden oluşturulur. Kategorileri tutar
+        temp => Datanın içerisinde filtrelenen dataları tutar 
+        catNameData => gelen datanın içinden oluşturulur. Tekilleştirilmiş kategorileri tutar
+        brandNameData => gelen datanın içinden oluşturulur. Tekilleştirilmiş marka isimlerinin baş harflerini tutar
+        activeCatID => seçili kategori idsini tutar. Varsayılan -1 yani hepsini gösterir
+        activeBrandID => seçili marka isminin boş harfini tutar. Varsayılan -1 yani hepsini gösterir
+
     */
     constructor(props) {
         super(props);
@@ -21,64 +26,100 @@ class Brands extends PureComponent {
             data: [],
             temp: [],
             catNameData: [],
-            brandNameData: []
+            brandNameData: [],
+            activeCatID: 21885, // aksesuar kategorisi
+            activeBrandID: -1
         };
     }
 
     componentDidMount() {
         const _self = this;
         _self.setState({ data: Brand }, () => {
-            _self._setData();
+            _self._setCategoryData();
+            _self._setBrandData();
+            _self._setFilterData();
         });
     }
 
     /* 
-        kategori ve marka isimlerini tekilleştirmek için kullanacağız
+        marka ve kategori filtresine göre kalan datayı süzer
     */
-    _setData = () => {
+    _setFilterData = () => {
         const _self = this,
-            { data = [] } = _self.state,
-            obj = {
-                catName: {
-
-                },
-                title: {
-
-                }
-            },
-            brandNameData = [],
-            catNameData = [];
+            { data = [], activeCatID = -1, activeBrandID = -1 } = _self.state,
+            arr = [];
 
         Object
             .entries(data)
             .forEach(([ind, value]) => {
-                let { catName, catID, title } = value,
-                    k = obj['catName'][catName] || '';
+                const { catID, title } = value;
+                if ((activeCatID == -1 || catID == activeCatID) && (activeBrandID == -1 || title.substr(0, 1) == activeBrandID))
+                    arr.push(value);
+            });
 
-                /* 
-                    kategori isimlerini tekilleştirme
-                */
-                if (k == '') {
-                    obj['catName'][catName] = catID;
-                    catNameData.push({ key: catName, value: catID });
+        console.log(arr);
+
+        _self.setState({ temp: arr });
+    }
+
+    /* 
+        marka isimlerinin ilk harflerini tekilleştirmek için kullanacağız burada önemli olan eğer herhangi bir kategori seçiliyse markalarında süzülmesi
+
+        ex: ['Loreal', 'Nivea', 'Nivea', 'Loreal']
+        result: ['L', 'N']  
+    */
+    _setBrandData = () => {
+        const _self = this,
+            { data = [], activeCatID = -1 } = _self.state,
+            obj = {},
+            arr = [];
+
+        Object
+            .entries(data)
+            .forEach(([ind, value]) => {
+                let { catID, title } = value;
+                if (activeCatID == -1 || catID == activeCatID) {
+                    title = title.substr(0, 1);
+                    const k = obj[title] || '';
+                    if (k == '') {
+                        obj[title] = title;
+                        arr.push({ key: title, value: title });
+                    }
                 }
+            });
 
-                /* marka isimlerini tekilleştirme  */
-                title = title.substr(0, 1)
-                k = obj['title'][title] || '';
+        arr.unshift({ key: 'Tümü', value: -1 });
+
+        _self.setState({ brandNameData: arr });
+    }
+
+    /* 
+        Gelen datadan kategori isimlerini tekileştiririz. 1 defa çalışmalıdır
+        ex: ['Makyaj', 'Makyaj','Makyaj','Makyaj','Cilt Bakım','Makyaj']
+        result: ['Cilt Bakım','Makyaj']
+    */
+    _setCategoryData = () => {
+        const _self = this,
+            { data = [] } = _self.state,
+            obj = {},
+            arr = [];
+
+        Object
+            .entries(data)
+            .forEach(([ind, value]) => {
+                let { catName, catID } = value,
+                    k = obj[catName] || '';
+
                 if (k == '') {
-                    obj['title'][title] = title;
-                    brandNameData.push({ key: title, value: title });
+                    obj[catName] = catID;
+                    arr.push({ key: catName, value: catID });
                 }
 
             });
 
-        catNameData.unshift({ key: 'Tümü', value: -1 });
-        brandNameData.unshift({ key: 'Tümü', value: -1 });
+        arr.unshift({ key: 'Tümü', value: -1 });
 
-        console.log(catNameData, brandNameData);
-
-        _self.setState({ catNameData: catNameData, brandNameData: brandNameData });
+        _self.setState({ catNameData: arr });
     }
 
     render() {
